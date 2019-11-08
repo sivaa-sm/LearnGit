0c7804e376df64ea9f05fb98647389e503e0efd0
sivaa-sm <sivaa.sm@gmail.com>
2019-11-08 19:54:13 +0530

    added new changes on TestLead.java

--- src/main/java/TestLead.java
+++ src/main/java/TestLead.java
@@ -7,6 +7,7 @@ public class TestLead implements Car{
 	public void getPrice(String carName, String model){
 		if(carName.equals("Suzuki") && "Alto".equals(model){
 			System.out.println("Test Lead Price : "+"4 lacs");
+			System.out.println("Test Lead, added new line");
 		}
 	}
 }
