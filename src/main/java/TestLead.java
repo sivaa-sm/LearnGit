d0d1cc3df0b27c616a2e51ede3ee8f89b3f0dc28
sivaa-sm <sivaa.sm@gmail.com>
2019-11-08 19:52:35 +0530

    Added TestLead.java

---
+++ src/main/java/TestLead.java
@@ -0,0 +1,14 @@
+public class TestLead implements Car{
+	public void getModel(String carName,int version){
+		if(carName.equals("Suzuki") && 1==version){
+			System.outprintln("Test lead carName :"+carName+"*** CarModel:"+"Alto MT");
+		}
+	}
+	public void getPrice(String carName, String model){
+		if(carName.equals("Suzuki") && "Alto".equals(model){
+			System.out.println("Test Lead Price : "+"4 lacs");
+		}
+	}
+}
+
+
